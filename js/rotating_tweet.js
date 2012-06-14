03fa3d05770d7d457e253bbe3a42adab63b2d4a7
mpntod <mpntod@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-14 15:19:00 +0000

    git-svn-id: https://plugins.svn.wordpress.org/rotatingtweets/trunk@558010 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/rotating_tweet.js
+++ js/rotating_tweet.js
@@ -4,8 +4,10 @@
 jQuery(document).ready(function() {
 	jQuery('.rotatingtweets').each(function() {
 		var rotate_id = "#"+this.id
+		var timeoutdelay = rotate_id.split('_');
 		jQuery(rotate_id).cycle({
 			pause: 1,
+			timeout: timeoutdelay[1],
 			fx: 'scrollUp'
 		});
 	});
