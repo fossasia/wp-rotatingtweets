b6d57e1be52c3a28eae729aa15f6209fce200e8f
mpntod <mpntod@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-04 13:54:34 +0000

    Neater loading of the Twitter script
    git-svn-id: https://plugins.svn.wordpress.org/rotatingtweets/trunk@581718 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/rotating_tweet.js
+++ js/rotating_tweet.js
@@ -12,3 +12,6 @@ jQuery(document).ready(function() {
 		});
 	});
 });
+/* And call the Twitter script while we're at it! */
+/* Standard script to call Twitter */
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
