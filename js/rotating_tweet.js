722e94cfa44306389dbe48732d0ebc3b874cc02c
mpntod <mpntod@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-06 12:23:14 +0000

    Allowing for different kinds of rotation
    git-svn-id: https://plugins.svn.wordpress.org/rotatingtweets/trunk@582241 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/rotating_tweet.js
+++ js/rotating_tweet.js
@@ -4,11 +4,23 @@
 jQuery(document).ready(function() {
 	jQuery('.rotatingtweets').each(function() {
 		var rotate_id = "#"+this.id
-		var timeoutdelay = rotate_id.split('_');
+		var rotate_id_split = rotate_id.split('_');
+		var rotate_timeout = rotate_id_split[1];
+		var rotate_fx = rotate_id_split[2];
+		/* If we have the zeebizcard template - set a minimum height - used to do this via a separate script, but this is easier to maintain */
+		if (jQuery('#zee_stylesheet-css').is('link')) {
+			var rotate_height = '7em';
+		} else {
+			var rotate_height = 'auto';
+		}
+		/* If the rotation type has not been set - then set it to scrollUp */
+		if(rotate_fx == null){rotate_fx = 'scrollUp'};
+		/* Call the rotation */
 		jQuery(rotate_id).cycle({
 			pause: 1,
-			timeout: timeoutdelay[1],
-			fx: 'scrollUp'
+			height: rotate_height,
+			timeout: rotate_timeout,
+			fx: rotate_fx
 		});
 	});
 });
