de971105ab54c7dd2f5be7769dd3a8dc09077cd5
mpntod <mpntod@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-19 16:56:48 +0000

    Minor tidying
    git-svn-id: https://plugins.svn.wordpress.org/rotatingtweets/trunk@587422 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rotatingtweets.php
+++ rotatingtweets.php
@@ -637,8 +637,7 @@ function rotating_tweets_display($json,$args,$print=TRUE) {
 						$result .= "\n\t<div class='rtw_meta'><div class='rtw_intents'>".rotatingtweets_intents($twitter_object,$twitterlocale, TRUE).'</div>';
 						$result .= rotatingtweets_timestamp_link($twitter_object,'long');
 						if($retweeter) {
-							$result .= " &middot; ".rotatingtweets_user_intent($retweeter,$twitterlocale,sprintf(__('Retweeted by %s'),$retweeter->name));
-#							$result .= " &middot; <a href='https://twitter.com/intent/user?user_id=".$retweeter->id."' title='{$retweeter->name}'>".sprintf(__('Retweeted by %s'),$retweeter->name).'</a>';
+							$result .= " &middot; ".rotatingtweets_user_intent($retweeter,$twitterlocale,"<img src='".plugins_url('images/retweet_on.png',__FILE__)."' width='16' height='16' align='bottom' alt='".sprintf(__('Retweeted by %s','rotatingtweets'),$retweeter->name)."' />".sprintf(__('Retweeted by %s','rotatingtweets'),$retweeter->name));
 						}
 						$result .= "\n</div>";
 						break;
@@ -654,7 +653,7 @@ function rotating_tweets_display($json,$args,$print=TRUE) {
 						$result .= "\n\t<p class='rtw_main'>".$main_text."</p>";
 						$result .= "\n\t<div class='rtw_meta'><div class='rtw_intents'>".rotatingtweets_intents($twitter_object,$twitterlocale, TRUE).'</div>';
 						if($retweeter) {
-							$result .= rotatingtweets_user_intent($retweeter,$twitterlocale,sprintf(__('Retweeted by %s'),$retweeter->name));
+							$result .= rotatingtweets_user_intent($retweeter,$twitterlocale,"<img src='".plugins_url('images/retweet_on.png',__FILE__)."' width='16' height='16' align='bottom' alt='".sprintf(__('Retweeted by %s','rotatingtweets'),$retweeter->name)."' />".sprintf(__('Retweeted by %s','rotatingtweets'),$retweeter->name));
 						}
 						$result .= "</div></div></div>";
 						break;
