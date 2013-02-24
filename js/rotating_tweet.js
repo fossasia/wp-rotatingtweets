c809161b1eeb1a49087cda5c1827f909b1f2be67
mpntod <mpntod@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-24 23:57:08 +0000

    And now updating the JavaScript
    git-svn-id: https://plugins.svn.wordpress.org/rotatingtweets/trunk@672750 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/rotating_tweet.js
+++ js/rotating_tweet.js
@@ -12,7 +12,7 @@ jQuery(document).ready(function() {
 		if(rotate_fx == null){rotate_fx = 'scrollUp'};
 		var rt_height_px = 'auto';
 		/* Now find the widget container width */
-		var rt_target_width = jQuery(this).closest('.widget_rotatingtweets_widget').width();
+		var rt_target_width = jQuery(this).closest('.widget').width();
 		var rt_fit = 1;
 		if( rt_target_width == 0 ) {
 			rt_target_width = 'auto';
