f5ec328ca8c760e06e7915c5eeea288bcd0d507a
mpntod <mpntod@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-30 10:58:05 +0000

    Support for 'digital catapult'
    git-svn-id: https://plugins.svn.wordpress.org/rotatingtweets/trunk@1016614 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rotatingtweets.php
+++ rotatingtweets.php
@@ -2018,6 +2018,13 @@ function rotatingtweets_enqueue_scripts() {
 				'jquery-cycle2-scrollvert' => plugins_url('cyclone-slider-2/libs/cycle2/jquery.cycle2.scrollVert.min.js'),
 				'rotating_tweet' => plugins_url('js/rotatingtweets_v2_cyclone.js', __FILE__)
 			);
+		elseif ( $style == 'digital catapult' || $style == 'digitalcatapult' ):
+			$rt_enqueue_script_list = array(
+				'jquery-cycle2' => get_template_directory_uri() . '/scripts/jquery.cycle2.min.js' ,
+				'jquery-cycle2_scrollvert' => plugins_url('js/jquery.cycle2.scrollVert.js', __FILE__),
+				'jquery-cycle2_carousel' => plugins_url('js/jquery.cycle2.carousel.js', __FILE__),
+				'rotating_tweet' => plugins_url('js/rotatingtweets_v2_cyclone.js', __FILE__)
+			);		
 		elseif ( function_exists( 'newswire_custom_scripts' ) ):
 			$rt_enqueue_script_list = array(
 				'cycle2' => get_template_directory_uri() . '/library/js/jquery.cycle2.min.js' ,
