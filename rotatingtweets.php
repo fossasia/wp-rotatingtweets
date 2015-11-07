2bc9dbc6c00aca3ee0561f4f6bea04a7c0d6d9d5
mpntod <mpntod@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-07 14:53:46 +0000

    Possible fix to timestamp
    git-svn-id: https://plugins.svn.wordpress.org/rotatingtweets/trunk@1281672 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rotatingtweets.php
+++ rotatingtweets.php
@@ -509,7 +509,8 @@ function rotatingtweets_timestamp_link($twitter_object,$timetype = 'default',$ta
 			$string .= rotatingtweets_contextualtime_short($tweettimestamp);
 			break;
 		case 'long':
-			$string .= date_i18n(get_option('time_format'),$tweettimestamp )." &middot; ".date_i18n(get_option('date_format') ,$tweettimestamp );
+			$timeoffset = get_option('gmt_offset');  // Correcting for Wordpress's timestamp setting
+			$string .= date_i18n(get_option('time_format'),$tweettimestamp + $timeoffset * 3600 )." &middot; ".date_i18n(get_option('date_format') ,$tweettimestamp  + $timeoffset * 3600 );
 			break;
 		default:
 			$string .= ucfirst(rotatingtweets_contextualtime($tweettimestamp));
